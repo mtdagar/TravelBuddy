cb141730c44dcbfb8e76b93fb1a0509fcf8d32d8
mtdagar <mtdagar@gmail.com>
2019-01-08 22:53:05 +0530

    Initial files added.

---
+++ index.php
@@ -0,0 +1,54 @@
+<html>
+<head>
+	<title>Welcome to Travel Buddy</title>
+	<!-- Bootstrap CDN -->
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+	<!-- Google font -->
+	<link href="https://fonts.googleapis.com/css?family=Poppins" rel="stylesheet">
+	<!-- Custom Stylesheet -->
+	<link href="assets/css/style.css" rel="stylesheet">
+</head>
+
+
+<body>
+
+	<!-- Bootstrap 4 dependencies -->
+	<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+	<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+
+
+	<!-- Navigation Bar -->
+        <nav class="navbar navbar-expand-lg transparent navbar-dark fixed-top">
+          <a class="navbar-brand" href="#">Navbar</a>
+          <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse" id="navbarNavDropdown">
+            <ul class="navbar-nav ml-auto">
+              <li class="nav-item active">
+                <a class="nav-link" href="#">ABOUT<span class="sr-only">(current)</span></a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">LOGIN</a>
+              </li>
+              
+            </ul>
+          </div>
+        </nav>
+
+
+  <!-- Banner -->    
+	<div id="mainbanner" class="view">
+        <div class="full-bg-img">
+        	<div class="banner">
+        		<h1 class="display-2">Fall in love with<br> your commute.</h1>
+        		<a href="#" class="transparent-btn">SIGN UP NOW</a>
+        		<span class="transparent"
+        	</div>
+
+        </div>   
+    </div>
+
+</body>
+</html>
