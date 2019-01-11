ecd96026f01e254341859d57c5a87554662d4f37
mtdagar <mtdagar@gmail.com>
2019-01-12 03:01:54 +0530

    Modified prototype of main page

--- main.php
+++ main.php
@@ -5,6 +5,14 @@
 <head>
 	<meta charset="UTF-8">
 	<title>TravelBuddy</title>
+
+	<!-- Bootstrap CDN -->
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+	
+	<!-- Google font -->
+	<link href="https://fonts.googleapis.com/css?family=Poppins" rel="stylesheet">
+
+	<!-- Custom stylesheet -->
 	<link rel="stylesheet" href="assets/css/main-page.css">
 </head>
 <body>
