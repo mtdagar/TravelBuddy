ca58a639aec0e88a577dafdcfec6f4293ce6b546
mtdagar <mtdagar@gmail.com>
2019-01-16 05:45:27 +0530

    Added appropriate registration fields

--- register.php
+++ register.php
@@ -69,14 +69,16 @@
 
     <!-- Form -->
     <form id="msform" method="post" action="actions.php">
+
+    	<!-- Step 1 -->
 		<fieldset class="container form-container">
 			<h2>Create an account</h2>
-			  <div class="form-row">
+			  <div class="form-row" style="padding-top: 20px; padding-bottom: 20px">
 			    <div class="form-group col-md-6">
-			      <input type="text" class="form-control margin-bottom" id="fname" placeholder="First Name">
+			      <input type="text" class="form-control" id="fname" placeholder="First Name">
 			    </div>
 			    <div class="form-group col-md-6">
-			      <input type="text" class="form-control margin-bottom" id="lname" placeholder="Last Name">
+			      <input type="text" class="form-control" id="lname" placeholder="Last Name">
 			    </div>
 			  </div>
 			  <div class="form-group">
@@ -88,40 +90,25 @@
 			  <button type="button" name ="next" class="btn btn-primary btn-block next">Next</button>
 		</fieldset>
 
+		<!-- Step 2 -->
 		<fieldset class="container form-container">
-			<h2>Enter your details</h2>
-			  <div class="form-row">
-			    <div class="form-group col-md-6">
-			      <input type="text" class="form-control margin-bottom" id="fname2" placeholder="First Name">
-			    </div>
-			    <div class="form-group col-md-6">
-			      <input type="text" class="form-control margin-bottom" id="lname2" placeholder="Last Name">
-			    </div>
+			<h2>How do you commute?</h2>
+			  </div>
+			  <div class="form-group" style="padding-top: 20px;">
+			    <input type="text" class="form-control margin-bottom" id="startingPoint" placeholder="Starting point">
 			  </div>
 			  <div class="form-group">
-			    <input type="text" class="form-control margin-bottom" id="email2" placeholder="Email">
-			  </div>
-			  <div class="form-group">
-			    <input type="Password" class="form-control margin-bottom" id="password2" placeholder="Password">
+			    <input type="text" class="form-control margin-bottom" id="destination" placeholder="Destination">
 			  </div>
 			  <button type="button" name ="next" class="btn btn-primary btn-block next">Next</button>
 		</fieldset>
 
+
+		<!-- Step 3 -->
 		<fieldset class="container form-container">
-			<h2>How do you commute?</h2>
-			  <div class="form-row">
-			    <div class="form-group col-md-6">
-			      <input type="text" class="form-control margin-bottom" id="fname3" placeholder="First Name">
-			    </div>
-			    <div class="form-group col-md-6">
-			      <input type="text" class="form-control margin-bottom" id="lname3" placeholder="Last Name">
-			    </div>
-			  </div>
-			  <div class="form-group">
-			    <input type="text" class="form-control margin-bottom" id="email3" placeholder="Email">
-			  </div>
-			  <div class="form-group">
-			    <input type="Password" class="form-control margin-bottom" id="password3" placeholder="Password">
+			<h3>Something about yourself</h3>
+			  <div class="form-group" style="padding-top: 20px; padding-bottom: 20px">
+			    <textarea class="form-control" id="bio" rows="3"></textarea>
 			  </div>
 			  <button type="submit" name ="submit" class="btn btn-primary btn-block submit">Sign up</button>
 		</fieldset>
