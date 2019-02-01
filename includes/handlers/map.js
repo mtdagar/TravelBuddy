53685d147a759861264b78104ab66511cb34ec71
apythoncharmer <apythoncharmer@gmail.com>
2019-02-01 18:03:31 +0530

    added 4 markers

--- includes/handlers/map.js
+++ includes/handlers/map.js
@@ -1,13 +1,14 @@
 //----------------------------------------------------------------------------------------------------------
-                                                  //Variables used
+												  //Variables used
 //----------------------------------------------------------------------------------------------------------
 
 	//start and end point coordinates obtained from registration page
-	var x= {lat: 28.7041, lng: 77.1025},
-	 	  y= {lat: 28.4595, lng: 77.0266},
-	  	i= {lat: 28.720459926425686, lng: 77.06609829069782}
-	  	j= {lat: 28.654611572607255, lng: 77.19773799758428};
-	    
+	var xs= {lat: 28.7041, lng: 77.1025},
+		  xe= {lat: 28.4595, lng: 77.0266},
+			ys= {lat: 28.720459926425686, lng: 77.06609829069782}
+			ye= {lat: 28.63545040536429, lng: 77.09682233886724};
+
+		
 	var platform = new H.service.Platform({
 		'app_id': 'RGqajGPjg7Rq21YzcF7V', 
 	  'app_code': 'OmWPbo_ai_qFr8ONOLEDZg'
@@ -16,7 +17,7 @@
 	var map = new H.Map(document.getElementById('mapContainer'),maptypes.terrain.map,
 	{
 	  zoom: 11,
-	  center: {lat: (x.lat+y.lat)/2, lng: (x.lng+y.lng)/2}
+	  center: {lat: (xs.lat+xe.lat)/2, lng: (xs.lng+xe.lng)/2}
 	});
 	var ui = H.ui.UI.createDefault(map, maptypes);
 	var mapEvents = new H.mapevents.MapEvents(map);
@@ -28,105 +29,107 @@
 	var rendicon = new H.map.Icon('assets/icons/routeendmarker.png');
 
 //----------------------------------------------------------------------------------------------------------
-                                                  //Adding markers
+												  //Adding markers
 //----------------------------------------------------------------------------------------------------------
    
-	function addDraggableMarkers(map, behavior,start_0,end_0){
+	function addDraggableMarkers(map, behavior,start_0,end_0,start_1, end_1){
 
-		var startmarker = new H.map.Marker(start_0, {icon: starticon});
-	  startmarker.draggable = true;
-	  map.addObject(startmarker);
-	        
-	  var endmarker = new H.map.Marker(end_0, {icon: endicon});
-	  endmarker.draggable = true;
-	  map.addObject(endmarker);
-	      
+		var startmarker0 = new H.map.Marker(start_0, {icon: starticon});
+	  startmarker0.draggable = true;
+	  map.addObject(startmarker0);
+			
+	  var endmarker0 = new H.map.Marker(end_0, {icon: endicon});
+	  endmarker0.draggable = true;
+	  map.addObject(endmarker0);
+
+	  var startmarker1 = new H.map.Marker(start_1, {icon: starticon});
+		map.addObject(startmarker1);
+
+		var endmarker1 = new H.map.Marker(end_1, {icon: endicon});
+		map.addObject(endmarker1);
+
+		  
 	  map.addEventListener('dragstart', function(ev) {
-	    var target = ev.target;
-	    if (target instanceof H.map.Marker) {
-	      behavior.disable();
-	  		}
-	    }, false);
+		var target = ev.target;
+		if (target instanceof H.map.Marker) {
+		  behavior.disable();
+			}
+		}, false);
 	  // re-enable the default draggability of the underlying map
 	  // when dragging has completed
 	  map.addEventListener('dragend', function(ev) {
-	    var target = ev.target;
-	    if (target instanceof mapsjs.map.Marker) {
-	      behavior.enable();
-	    }
+		var target = ev.target;
+		if (target instanceof mapsjs.map.Marker) {
+		  behavior.enable();
+		}
 	  }, false);
 
 	  // Listen to the drag event and move the position of the marker
 	  // as necessary
 	  map.addEventListener('drag', function(ev) {
-	    var target = ev.target,
-	        pointer = ev.currentPointer;
-	    if (target instanceof mapsjs.map.Marker) {
-	      target.setPosition(map.screenToGeo(pointer.viewportX, pointer.viewportY));
-	      if(target == startmarker)
-	        x=map.screenToGeo(pointer.viewportX, pointer.viewportY);
-	      else if(target == endmarker)
-	        y=map.screenToGeo(pointer.viewportX, pointer.viewportY);
-	    }
+		var target = ev.target,
+			pointer = ev.currentPointer;
+		if (target instanceof mapsjs.map.Marker) {
+		  target.setPosition(map.screenToGeo(pointer.viewportX, pointer.viewportY));
+		  if(target == startmarker0)
+			xs=map.screenToGeo(pointer.viewportX, pointer.viewportY);
+		  else if(target == endmarker0)
+			xe=map.screenToGeo(pointer.viewportX, pointer.viewportY);
+		}
 	  }, false);
 	}
 
 //----------------------------------------------------------------------------------------------------------
-//                                        Finding the route between the markers
+												  //Finding the route between the markers
 //----------------------------------------------------------------------------------------------------------
 
-	function findroute(start_0, end_0){
+	function findroute(start_0, end_0, start_1, end_1){
 	  
-	  var routingParameters = {
-	    'mode': 'fastest;car',
-	    'waypoint0': start_0.lat+','+start_0.lng,
-	    'waypoint1': end_0.lat+','+end_0.lng,
-	    'representation': 'display'
+	  var routingParameters0 = {
+		'mode': 'fastest;car',
+		'waypoint0': start_0.lat+','+start_0.lng,
+		'waypoint1': end_0.lat+','+end_0.lng,
+		'representation': 'display'
 	  };
 
 	  // Define a callback function to process the routing response:
 	  var onResult = function(result) {
-	    var route,
-	    	routeShape,
-	      startPoint,
-	      endPoint,
-	      linestring;
-	    if(result.response.route) {
-	    // Pick the first route from the response:
-	    route = result.response.route[0];
-	    // Pick the route's shape:
-	    routeShape = route.shape;
+			
+			var route,
+			routeShape,
+		  startPoint,
+		  endPoint,
+		  linestring;
 
-	    // Create a linestring to use as a point source for the route line
-	    linestring = new H.geo.LineString();
+			if(result.response.route) {
+				// Pick the first route from the response:
+				route = result.response.route[0];
+				// Pick the route's shape:
+				routeShape = route.shape;
 
-	    // Push all the points in the shape into the linestring:
-	    routeShape.forEach(function(point) {
-	      var parts = point.split(',');
-	      linestring.pushLatLngAlt(parts[0], parts[1]);  
-	    });
-	    console.log(linestring)
-	    
-/*
-	    // Retrieve the mapped positions of the requested waypoints:
-	    startPoint = route.waypoint[0].mappedPosition;
-	    endPoint = route.waypoint[1].mappedPosition;
-*/
-	    // Create a polyline to display the route:
-	    var routeLine = new H.map.Polyline(linestring, {
-	      style: { strokeColor: 'rgba(253, 151, 114, 1)', lineWidth: 4 }
-	    });
+				// Create a linestring to use as a point source for the route line
+				linestring = new H.geo.LineString();
 
+				// Push all the points in the shape into the linestring:
+				routeShape.forEach(function(point) {
+				  var parts = point.split(',');
+				  linestring.pushLatLngAlt(parts[0], parts[1]);  
+				});
 
-			var startMarker = new H.map.Marker(start_0, {icon: rstarticon});
-			var endMarker = new H.map.Marker(end_0, {icon: rendicon});
+				// Create a polyline to display the route:
+				var routeLine = new H.map.Polyline(linestring, {
+				  style: { strokeColor: 'rgba(253, 151, 114, 1)', lineWidth: 4 }
+				});
 
-	    // Add the route polyline and the two markers to the map:
-	    map.addObjects([routeLine, startMarker, endMarker]);
-	    // Set the map's viewport to make the whole route visible:
-	    map.setViewBounds(routeLine.getBounds());
-	    }
-	  };
+				var startMarker = new H.map.Marker(start_0, {icon: rstarticon});
+				var endMarker = new H.map.Marker(end_0, {icon: rendicon});
+
+				// Add the route polyline and the two markers to the map:
+				map.addObjects([routeLine, startMarker, endMarker]);
+				// Set the map's viewport to make the whole route visible:
+				map.setViewBounds(routeLine.getBounds());
+			}
+	};
 
 	  // Get an instance of the routing service:
 	  var router = platform.getRoutingService();
@@ -134,10 +137,10 @@
 	  // Call calculateRoute() with the routing parameters,
 	  // the callback and an error callback function (called if a
 	  // communication error occurs):
-	  router.calculateRoute(routingParameters, onResult,
-	    function(error) {
-	      alert(error.message);
-	    });
+	  router.calculateRoute(routingParameters0, onResult,
+		function(error) {
+		  alert(error.message);
+		});
 	}
 
 
@@ -146,5 +149,5 @@
 //----------------------------------------------------------------------------------------------------------
 //                                               Function Calls
 //----------------------------------------------------------------------------------------------------------
-addDraggableMarkers(map,behavior,x,y,i,j); 
-findroute(x,y,i,j);
+addDraggableMarkers(map,behavior,xs,xe,ys,ye); 
+//findroute(xs,xe,ys,ye);
