d11b131a4b43e7ce0797bbfc8e7a121ff16ec9df
mtdagar <mtdagar@gmail.com>
2019-02-06 00:26:20 +0530

    Added user controls to registration pages

--- registerBio.php
+++ registerBio.php
@@ -50,6 +50,11 @@
 
 <body>
 
+	<!-- Bootstrap 4 dependencies -->
+	<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+	<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+
 	<!-- Navigation Bar -->
     <nav class="navbar navbar-expand-lg transparent navbar-dark fixed-top">
        	<a class="navbar-brand" href="index.php"><img src="assets/icons/logo.png" alt="travelBuddy"></a>
@@ -58,13 +63,31 @@
        	</button>
        	<div class="collapse navbar-collapse" id="navbarNavDropdown">
             <ul class="navbar-nav ml-auto" id="navList">
-              <li class="nav-item active" id="loginLinkItem">
-                <a class="nav-link" id="loginLink" href="login.php">LOGIN</a>
-              </li>
+                
+                <?php if(isset($_SESSION['userLoggedIn']) && !empty($_SESSION['userLoggedIn'])) : ?> 
+
+                <li class="nav-item dropdown">
+                  <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                    user
+                  </a>
+                  <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                    <a class="dropdown-item" href="#">Your profile</a>
+                    <div class="dropdown-divider"></div>
+                    <a class="dropdown-item" href="includes/classes/Logout.php">Sign out</a>
+                  </div>
+                </li> 
+
+              <?php else : ?>  
+
+                <li class="nav-item" id="loginLinkItem">
+                  <a class="nav-link" id="loginLink" href="login.php">Login</a>
+                </li>
+
+              <?php endif; ?>
+
               <li class="nav-item">
-                <a class="nav-link" id="aboutLink" href="#">ABOUT<span class="sr-only">(current)</span></a>
+                <a class="nav-link" id="aboutLink" href="#">About<span class="sr-only">(current)</span></a>
               </li>
-              
             </ul>
        	</div>
     </nav>
@@ -101,21 +124,7 @@
 		</div>
 			  			
 	</form>
-
-
-
-	<!-- jQuery -->
-  	<script src='http://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js'></script>
-	<script src='http://cdnjs.cloudflare.com/ajax/libs/jquery-easing/1.3/jquery.easing.min.js'></script>
 	
-
-	<?php
-
-	  if(isset($_SESSION['userLoggedIn']) && !empty($_SESSION['userLoggedIn'])) {
-	    echo "<script src='includes/handlers/nav-links.js' type='text/javascript'></script>";
-	  }
-
-	?>
 
 </body>
 
