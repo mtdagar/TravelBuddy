a5b52ef45dc9ebbc0547ac316b2074ebefaa1635
mtdagar <mtdagar@gmail.com>
2019-02-11 22:38:59 +0530

    Added routing between two points

--- includes/handlers/map.js
+++ includes/handlers/map.js
@@ -1,11 +1,35 @@
 function initMap() {
+	var directionsService = new google.maps.DirectionsService;
+    var directionsDisplay = new google.maps.DirectionsRenderer;
+
     // The location of Uluru
     var uluru = {lat: -25.344, lng: 131.036};
     // The map, centered at Uluru
-    var map = new google.maps.Map(
-        document.getElementById('map'), {zoom: 4, center: uluru});
+    var map = new google.maps.Map(document.getElementById('map'), {
+          zoom: 7,
+          center: {lat: 41.85, lng: -87.65}
+        });
+
+    directionsDisplay.setMap(map);
+    calculateAndDisplayRoute(directionsService, directionsDisplay);
     // The marker, positioned at Uluru
     var marker = new google.maps.Marker({position: uluru, map: map});
 }
 
+function calculateAndDisplayRoute(directionsService, directionsDisplay) {
+    directionsService.route({
+        origin: 'Rohini, New Delhi, Delhi, India',
+        destination: 'Gurugram, Delhi',
+        travelMode: 'DRIVING'
+        }, function(response, status) {
+          if (status === 'OK') {
+            directionsDisplay.setDirections(response);
+          } else {
+            window.alert('Directions request failed due to ' + status);
+          }
+        }
+    );
+}
+
+//function calls
 initMap();
