1872a8c58366b032a4ecc7b2eab4e04bb113861b
Dikshant Rawat <dikshantrawat0@gmail.com>
2019-03-19 02:21:37 +0530

    Added Profile Page

---
+++ profile.php
@@ -0,0 +1,28 @@
+<html>
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <link rel="stylesheet" href="assets/css/profile.css">
+  <title>Profile Page</title>
+</head>
+<body>
+<div class="info">
+    <div id="displayImage"><img src="assets/images/displayImage.jpeg" alt="Display Image"></div>
+    <div class="name"><h1>Lorem Ipsum</h1></div>
+  <div class="buttons">
+    <div class="message"><button type="button">Message</button></div>
+    <div class="friend"><button type="button">Add Buddy</button></div>
+  </div>
+</div>
+<div class="posts">
+  <div class="image"><img src="assets/images/1.jpeg" alt=""></div>
+  <div class="image"><img src="assets/images/2.jpeg" alt=""></div>
+  <div class="image"><img src="assets/images/3.jpeg" alt=""></div>
+  <div class="image"><img src="assets/images/4.png" alt=""></div>
+  <div class="image"><img src="assets/images/5.jpeg" alt=""></div>
+  <div class="image"><img src="assets/images/6.jpeg" alt=""></div>
+</div>
+
+</body>
+</html>
